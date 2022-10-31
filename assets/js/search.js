2be38ef535d1188f92c26eed14062cd7cb49abb5
Kadigas <adbagas05@gmail.com>
2022-11-01 00:16:14 +0700

    feat: 404 page

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,5 +1,9 @@
 function search(){
     var c = document.getElementById("code").value;
-    if(c == 'QK6AJI6S')
+    if(c == 'QK6AJI6S') {
         window.location.href="cari/QK6AJI6S.html";
+    }
+    else {
+        window.location.href="cari/404.html";
+    }
 }
